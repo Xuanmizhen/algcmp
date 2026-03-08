c6b7691085fc353f4a91eb1b96ac7e5bd535842b
Xuanmizhen <xuanmizhen@icloud.com>
2026-03-08 10:49:40 +0800

    Add disjoint_set class implementation with find_set and unite methods

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
+// C++20
+// PTA: C++ (g++)
+
+#include <compare> // std::strong_ordering
 #include <cstdint>
 #include <fstream>
 #include <iostream>
 #include <type_traits> // std::make_signed_t
+#include <vector>
 
 using i16 = int16_t;
 using i32 = int32_t;
@@ -19,11 +24,47 @@ using std::cin;
 using std::cout;
 using std::cerr;
 
+
 #define dbg(val) \
     ([](const auto v) { \
         cerr << "[" __FILE__ ":" << __LINE__ << "] " #val " = " << v << '\n'; \
         return v; \
     })(val)
+
+class disjoint_set {
+    std::vector<isize> parent_or_neg_rank;
+
+public:
+    explicit disjoint_set(const usize n) : parent_or_neg_rank(n, -1) { }
+    usize find_set(usize x) {
+        auto root = x;
+        while (parent_or_neg_rank[root] >= 0) {
+            root = parent_or_neg_rank[root];
+        }
+        while (x != root) {
+            const auto next = parent_or_neg_rank[x];
+            parent_or_neg_rank[x] = root;
+            x = next;
+        }
+        return root;
+    }
+    bool unite(usize x, usize y) {
+        if ((x = find_set(x)) == (y = find_set(y))) {
+            return true;
+        }
+        const auto cmp{parent_or_neg_rank[x] <=> parent_or_neg_rank[y]};
+        if (cmp < 0) {
+            parent_or_neg_rank[y] = x;
+        } else {
+            if (cmp == 0) {
+                --parent_or_neg_rank[y];
+            }
+            parent_or_neg_rank[x] = y;
+        }
+        return false;
+    }
+};
+
 
 void run() {
     u32 t;
