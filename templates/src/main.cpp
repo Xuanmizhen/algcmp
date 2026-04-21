f2dc85e020da9a81277b5c1dbf1971aa991c0af0
Xuanmizhen <xuanmizhen@icloud.com>
2026-04-21 15:51:11 +0800

    Support more Python functionalities

--- templates/src/main.cpp
+++ templates/src/main.cpp
@@ -3,6 +3,8 @@
 // PTA: C++ (g++)
 // QOJ.ac: C++20 or C++23 or C++26
 
+static_assert(__cplusplus >= 202002L);
+
 
 #include <cassert> // assert
 #include <compare> // std::strong_ordering
@@ -23,7 +25,14 @@
 // #include <bits/stdc++.h> // g++
 
 
-// 缩写和语法糖
+// 进口
+
+#define let const auto
+#define var auto
+#define fn auto
+#define loop for (;;)
+#define in :
+#define self (*this)
 
 using u16 = uint16_t;
 using i16 = int16_t;
@@ -35,29 +44,78 @@ using u128 = __uint128_t;
 using i128 = __int128;
 using usize = size_t;
 using isize = std::make_signed_t<usize>;
+template <typename T>
+using Option = std::optional<T>;
+template <class T>
+constexpr std::optional<std::decay_t<T>> Some(T &&value) {
+    return std::make_optional(value);
+}
+inline constexpr std::nullopt_t None{std::nullopt};
 
-using std::cin;
-using std::cout;
-using std::source_location;
-using std::views::iota;
+template <typename I>
+class Cycle;
 
-#define let const auto
-#define var auto
-#define fn auto
-#define loop for (;;)
-#define in :
+template <typename Derived, typename T>
+class Iterator {
+public:
+    using Item = T;
+
+    fn next() -> Option<Item> {
+        return static_cast<Derived *>(this)->next();
+    }
+
+    template <typename B, typename F>
+    fn fold(const B init, F f) -> B {
+        var accum = init;
+        while (let item = self.next()) {
+            accum = f(accum, item);
+        }
+        return accum;
+    }
+
+    template <typename F>
+    fn for_each(F f) {
+        while (let item = self.next()) {
+            f(item.value());
+        }
+    }
+
+    fn cycle() -> Cycle<Derived> {
+        return Cycle<Derived>(*static_cast<Derived *>(this), *static_cast<Derived *>(this));
+    }
+};
+
+template <typename I>
+class Cycle : public Iterator<Cycle<I>, typename I::Item> {
+    I orig;
+    I iter;
+    Cycle(I orig, I iter) : orig(orig), iter(iter) { }
+
+public:
+    template <typename Derived, typename T>
+    friend class Iterator;
+
+    fn next() -> Option<typename I::Item> {
+        if (let y = self.iter.next()) {
+            return y;
+        } else {
+            self.iter = self.orig;
+            return self.iter.next();
+        }
+    }
+};
 
 
 // 向 C++26 看齐
 
 #if __cplusplus <= 202302L
 // Computes the addition `x + y` and stores the result into `*result`. The addition is performed as if both operands were represented in a signed integer type with infinite range, and the result was then converted from this integer type to `type1`. If the value assigned to `*result` correctly represents the mathematical result of the operation, it returns `false`. Otherwise, it returns `true`. In this case, the value assigned to *result is the mathematical result of the operation wrapped around to the width of `*result`.
-template<std::unsigned_integral type1>
+template <std::unsigned_integral type1>
 fn ckd_add(type1 *result, type1 a, type1 b) -> bool {
     return __builtin_add_overflow(a, b, result);
 }
 // Computes the multiplication `x × y` and stores the result into `*result`. The multiplication is performed as if both operands were represented in a signed integer type with infinite range, and the result was then converted from this integer type to `type1`. If the value assigned to `*result` correctly represents the mathematical result of the operation, it returns `false`. Otherwise, it returns `true`. In this case, the value assigned to `*result` is the mathematical result of the operation wrapped around to the width of `*result`.
-template<std::unsigned_integral type1>
+template <std::unsigned_integral type1>
 fn ckd_mul(type1 *result, type1 a, type1 b) -> bool {
     return __builtin_mul_overflow(a, b, result);
 }
@@ -68,16 +126,16 @@ fn ckd_mul(type1 *result, type1 a, type1 b) -> bool {
 
 // 调试工具
 
-fn log_loc(std::ostream &os, const source_location loc = source_location::current()) -> std::ostream & {
+fn log_loc(std::ostream &os, const std::source_location loc = std::source_location::current()) -> std::ostream & {
     return os << '[' << loc.file_name() << ':' << loc.line() << ':' << loc.column() << "] `" << loc.function_name() << "`: ";
 }
 
 #ifdef LOCAL
 #define dbg(val) \
-    ([](const decltype((val)) v, const source_location loc) -> decltype((val)) { \
+    ([](const decltype((val)) v, const std::source_location loc) -> decltype((val)) { \
         log_loc(std::clog, loc) << " (" << #val << ") = " << v << std::endl; \
         return (v); \
-    })((val), source_location::current())
+    })((val), std::source_location::current())
 #define debug_assert(e) assert(e)
 #define todo() (throw std::runtime_error("not yet implemented"))
 #else
@@ -91,9 +149,9 @@ fn log_loc(std::ostream &os, const source_location loc = source_location::curren
 
 namespace std {
     namespace numbers {
-        template<typename T>
+        template <typename T>
         inline constexpr T tau_v = 2 * pi_v<T>;
-        template<typename T>
+        template <typename T>
         inline constexpr T inv_tau_v = inv_pi_v<T> / static_cast<T>(2.0);
         inline constexpr double tau = tau_v<double>;
         inline constexpr double inv_tau = inv_tau_v<double>;
@@ -101,43 +159,20 @@ namespace std {
 }
 
 
-// 线性代数
-
-template<typename T, typename Container = std::vector<std::vector<T>>>
-class matrix {
-public:
-    using row_type = Container::value_type;
-    using value_type = row_type::value_type;
-    static_assert(std::is_same_v<value_type, T>);
-    Container inner;
-
-    matrix() : matrix(Container()) { }
-    explicit matrix(const Container &cont) : inner(cont) { }
-    explicit matrix(Container &&cont) : inner(cont) { }
-    matrix(const usize n, const usize m) : inner(n, row_type(m)) { }
-    // reference operator[](usize pos) {
-    //     return inner[pos];
-    // }
-    // const_reference operator[](usize pos) const {
-    //     return inner[pos];
-    // }
-};
-
-
 // 并查集
 
 class disjoint_set {
     std::vector<isize> parent_or_neg_rank;
     fn link(usize x, usize y) -> uint8_t {
-        let cmp = parent_or_neg_rank[x] <=> parent_or_neg_rank[y];
+        let cmp = self.parent_or_neg_rank[x] <=> self.parent_or_neg_rank[y];
         if (cmp < 0) {
-            parent_or_neg_rank[y] = x;
+            self.parent_or_neg_rank[y] = x;
             return 0;
         }
         if (cmp == 0) {
-            --parent_or_neg_rank[y];
+            --self.parent_or_neg_rank[y];
         }
-        parent_or_neg_rank[x] = y;
+        self.parent_or_neg_rank[x] = y;
         return 1;
     }
 
@@ -145,28 +180,28 @@ public:
     explicit disjoint_set(const usize n) : parent_or_neg_rank(n, -1) { }
     fn find_set(usize x) -> usize {
         var root = x;
-        while (parent_or_neg_rank[root] >= 0) {
-            root = parent_or_neg_rank[root];
+        while (self.parent_or_neg_rank[root] >= 0) {
+            root = self.parent_or_neg_rank[root];
         }
         while (x != root) {
-            let next = parent_or_neg_rank[x];
-            parent_or_neg_rank[x] = root;
+            let next = self.parent_or_neg_rank[x];
+            self.parent_or_neg_rank[x] = root;
             x = next;
         }
         return root;
     }
     struct unite_result {
         usize union_repr;
-        std::optional<usize> deleted_repr;
+        Option<usize> deleted_repr;
     };
-    unite_result unite(usize x, usize y) {
-        if ((x = find_set(x)) == (y = find_set(y))) {
-            return {x, {}};
+    fn unite(usize x, usize y) -> unite_result {
+        if ((x = self.find_set(x)) == (y = self.find_set(y))) {
+            return {x, None};
         }
-        if (link(x, y) == 0) {
-            return {x, y};
+        if (self.link(x, y) == 0) {
+            return {x, Some(y)};
         } else {
-            return {y, x};
+            return {y, Some(x)};
         }
     }
 };
@@ -174,36 +209,36 @@ public:
 
 // 图论
 
-template<std::three_way_comparable W>
+template <std::three_way_comparable W>
 class undirected_edge {
 public:
     usize u, v;
     W weight;
 
     fn operator<=>(const undirected_edge &rhs) const -> std::weak_ordering {
-        return rhs.weight <=> weight;
+        return rhs.weight <=> self.weight;
     }
 };
 
-template<std::three_way_comparable W>
-fn kruskal_safe_edge(disjoint_set &components, std::priority_queue<undirected_edge<W>> &q) -> std::optional<undirected_edge<W>> {
+template <std::three_way_comparable W>
+fn kruskal_safe_edge(disjoint_set &components, std::priority_queue<undirected_edge<W>> &q) -> Option<undirected_edge<W>> {
     while (!q.empty()) {
         let e = q.top();
         q.pop();
         if (components.unite(e.u, e.v).deleted_repr.has_value()) {
-            return e;
+            return Some(e);
         }
     }
-    return std::nullopt;
+    return None;
 }
 
-template<typename C>
+template <typename C>
 struct action {
     usize dest;
     C cost;
 };
 
-template<typename W>
+template <typename W>
 class directed_edge {
 public:
     usize source;
@@ -214,7 +249,7 @@ public:
 
 
 // 快速幂
-template<std::integral T, std::unsigned_integral E>
+template <std::integral T, std::unsigned_integral E>
 fn powi(T base, E exp) -> T {
     var res = T(1);
     while (exp > 0) {
@@ -230,9 +265,9 @@ fn powi(T base, E exp) -> T {
 
 // 溢出标记
 
-template<std::unsigned_integral I>
+template <std::unsigned_integral I>
 class overflowable {
-    std::optional<I> inner;
+    Option<I> inner;
     overflowable() { }
 
 public:
@@ -240,8 +275,8 @@ public:
     fn overflowed() const -> bool {
         return !inner.has_value();
     }
-    fn value() -> std::optional<I> {
-        return inner;
+    fn value() -> Option<I> {
+        return self.inner;
     }
     fn operator++() -> overflowable & {
         if (!overflowed()) {
@@ -251,27 +286,27 @@ public:
                 ++inner.value();
             }
         }
-        return *this;
+        return self;
     }
     fn operator++(int) -> overflowable {
-        let original = *this;
-        ++*this;
+        let original = self;
+        ++self;
         return original;
     }
     fn operator+(const I val) const -> overflowable {
         if (!overflowed()) {
             I res;
-            return ckd_add(&res, inner.value(), val) ? overflowable() : overflowable(res);
+            return ckd_add(&res, self.inner.value(), val) ? overflowable() : overflowable(res);
         }
         return overflowable();
     }
     fn operator+=(const I val) -> overflowable & {
-        return *this = *this + val;
+        return self = self + val;
     }
     fn operator*(const I val) const -> overflowable {
         if (!overflowed()) {
             I res;
-            return ckd_mul(&res, inner.value(), val) ? overflowable() : overflowable(res);
+            return ckd_mul(&res, self.inner.value(), val) ? overflowable() : overflowable(res);
         }
         return overflowable();
     }
@@ -283,17 +318,17 @@ public:
         return overflowable();
     }
     fn operator*=(const I val) -> overflowable & {
-        return *this = *this * val;
+        return self = self * val;
     }
     fn operator*=(const overflowable &rhs) -> overflowable & {
-        return *this = *this * rhs;
+        return self = self * rhs;
     }
     fn operator<=>(const overflowable &rhs) const -> std::partial_ordering {
-        if (overflowed() && rhs.overflowed()) {
+        if (self.overflowed() && rhs.overflowed()) {
             // overflowable() cannot compare with itself
             return std::partial_ordering::unordered;
         }
-        if (overflowed()) {
+        if (self.overflowed()) {
             // this is overflowable(), greater than any I
             return std::partial_ordering::greater;
         }
@@ -301,14 +336,14 @@ public:
             // rhs is overflowable(), less than overflowable()
             return std::partial_ordering::less;
         }
-        return std::partial_ordering(inner.value() <=> rhs.inner.value());
+        return std::partial_ordering(self.inner.value() <=> rhs.inner.value());
     }
 };
 
 
 // 模意义下的计算
 
-template<std::unsigned_integral I, I M>
+template <std::unsigned_integral I, I M>
 class mod_unsigned {
     static_assert(M > 0, "M must be positive");
 
@@ -321,49 +356,49 @@ public:
     }
 
     fn operator==(const mod_unsigned &rhs) const -> bool {
-        return inner == rhs.inner;
+        return self.inner == rhs.inner;
     }
 
     fn operator++() -> mod_unsigned & {
-        if (++inner == M) {
-            inner = 0;
+        if (++self.inner == M) {
+            self.inner = 0;
         }
-        return *this;
+        return self;
     }
     fn operator++(int) -> mod_unsigned {
-        let original = *this;
-        ++*this;
+        let original = self;
+        ++self;
         return original;
     }
     fn operator+(const mod_unsigned &rhs) const -> mod_unsigned {
         static_assert(std::bit_width(M) + 1 <= std::numeric_limits<I>::digits, "M may be too large for +");
-        return mod_unsigned{(inner + rhs.inner) % M};
+        return mod_unsigned{(self.inner + rhs.inner) % M};
     }
     fn operator+=(const mod_unsigned &rhs) -> mod_unsigned & {
-        return *this = *this + rhs;
+        return self = self + rhs;
     }
 
     fn operator--() -> mod_unsigned & {
-        if (inner == 0) {
-            inner = M - 1;
+        if (self.inner == 0) {
+            self.inner = M - 1;
         } else {
-            --inner;
+            --self.inner;
         }
-        return *this;
+        return self;
     }
     fn operator--(int) -> mod_unsigned {
-        let original = *this;
-        --*this;
+        let original = self;
+        --self;
         return original;
     }
     fn operator-() const -> mod_unsigned {
-        return inner == 0 ? *this : mod_unsigned{M - inner};
+        return self.inner == 0 ? self : mod_unsigned{M - self.inner};
     }
     fn operator-(const mod_unsigned &rhs) const -> mod_unsigned {
-        return *this + -rhs;
+        return self + -rhs;
     }
     fn operator-=(const mod_unsigned &rhs) -> mod_unsigned & {
-        return *this += -rhs;
+        return self += -rhs;
     }
 
     fn operator*(const mod_unsigned &rhs) const -> mod_unsigned {
@@ -371,44 +406,44 @@ public:
         return mod_unsigned{(inner * rhs.inner) % M};
     }
     fn operator*=(const mod_unsigned &rhs) -> mod_unsigned & {
-        return *this = *this * rhs;
+        return self = self * rhs;
     }
 
     fn multiplicative_inverse() const -> mod_unsigned {
         // FIXME: Assert M to be prime.
-        return powi(*this, M - I{2});
+        return powi(self, M - I{2});
     }
     fn operator/=(const mod_unsigned &rhs) -> mod_unsigned & {
-        return *this *= rhs.multiplicative_inverse();
+        return self *= rhs.multiplicative_inverse();
     }
     fn operator/(const mod_unsigned &rhs) const -> mod_unsigned {
-        return *this * rhs.multiplicative_inverse();
+        return self * rhs.multiplicative_inverse();
     }
 };
 
 
 // 组合数学
 
-template<std::unsigned_integral I, typename R>
+template <std::unsigned_integral I, typename R>
 fn factorial(const I n) -> R {
-    let view = iota(I{1}, n + I{1});
-    return std::accumulate(view.begin(), view.end(), R{1}, [](const var acc, const var x) {
+    let view = std::views::iota(I{1}, n + I{1});
+    return std::accumulate(view.begin(), view.end(), R{1}, [](let acc, let x) {
         return acc * R{x};
     });
 }
 
-template<std::unsigned_integral I, typename R>
+template <std::unsigned_integral I, typename R>
 fn perm(const I n, const I k) -> R {
     if (k > n) {
         return R{0};
     }
-    let view = iota(n - k + I{1}, n + I{1});
-    return std::accumulate(view.begin(), view.end(), R{1}, [](const var acc, const var x) {
+    let view = std::views::iota(n - k + I{1}, n + I{1});
+    return std::accumulate(view.begin(), view.end(), R{1}, [](let acc, let x) {
         return acc * R{x};
     });
 }
 
-template<std::unsigned_integral I, typename R>
+template <std::unsigned_integral I, typename R>
 fn comb(const I n, const I k) -> R {
     return perm<I, R>(n, k) / factorial<I, R>(k);
 }
@@ -417,7 +452,7 @@ fn comb(const I n, const I k) -> R {
 // 最长公共子序列
 
 // longest-common-subsequence problem
-template<std::ranges::input_range R>
+template <std::ranges::input_range R>
 fn lcs(R &&a, R &&b) -> usize {
     usize cnt{0};
     todo();
@@ -449,6 +484,9 @@ fn test() {
 // 任务
 
 fn run() {
+    using std::cin;
+    using std::cout;
+
     u32 t;
     cin >> t;
     while (t-- > 0) {
--- templates/src/main.py
+++ templates/src/main.py
@@ -1,10 +1,45 @@
-import sys
 # from array import array
+import itertools
+import operator
+import sys
 
 
 input = sys.stdin.readline
 write = sys.stdout.write
 
-# t = int(input())
-# for _ in range(t):
-#     pass
+if sys.version_info >= (3, 12):
+    from itertools import batched
+    from math import sumprod
+
+else:
+    def batched(iterable, n, *, strict=False):
+        # batched('ABCDEFG', 3) → ABC DEF G
+        if n < 1:
+            raise ValueError('n must be at least one')
+        iterator = iter(iterable)
+        while batch := tuple(itertools.islice(iterator, n)):
+            if strict and len(batch) != n:
+                raise ValueError('batched(): incomplete batch')
+            yield batch
+
+    def sumprod(p, q):
+        "Note that in this version, if the inputs do not have the same length, the behavior is not specified. Also of note is that the precision for float and mixed int/float inputs is not guaranteed."
+        return sum(map(operator.mul, p, q))
+
+    if sys.version_info >= (3, 10):
+        from itertools import pairwise
+    else:
+        def pairwise(iterable):
+            # pairwise('ABCDEFG') → AB BC CD DE EF FG
+
+            iterator = iter(iterable)
+            a = next(iterator, None)
+
+            for b in iterator:
+                yield a, b
+                a = b
+
+
+t = int(input())
+for _ in range(t):
+    pass
